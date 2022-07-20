78c757b0f8d6e30a5748a9a3a4aed632291e439d
NiboshiDaifuku <53576995+NiboshiDaifuku@users.noreply.github.com>
2022-07-21 04:31:51 +0900

    ルール説明のモーダルを作成

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import "./css/App.css";
-import "./css/Tiles.scss";
+import { useState } from "react";
 import Tiles from "./Tiles";
+import Modal from "./Modal";
 import { getGameTheme } from "./lib/GameOperation";
 
+import "./css/App.css";
+import "./css/Tiles.scss";
+
 export default function App() {
+  const [showRuleFlag, setShowRuleModal] = useState(false);
+  const showRuleModal = () => {
+    setShowRuleModal(true);
+  };
+
   return (
     <div className="App">
       <h1>知的協力ゲーム「50音表をぬりつぶせ！」</h1>
       <h3>お題: {getGameTheme()}</h3>
+      <button onClick={showRuleModal}>ルール説明</button>
       <Tiles />
+      <Modal showFlag={showRuleFlag} setShowModal={setShowRuleModal} />
     </div>
   );
 }
